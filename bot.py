5d13f371545134098ee959965b35ee55b11d3124
aiartbookk-glitch <aiartbookk@gmail.com>
2026-03-04 12:32:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,33 @@
 import telebot
 import requests
-import os
 import io
 
-BOT_TOKEN = os.getenv("8495788801:AAH52uGWsD-OUoTDdZlV6oy8NnyduVOmyos")
-API_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0Z19pZCI6NTMxMDU1NTUzNSwiZGJfbm0iOiJzdWJfZGF0YTIyIn0.lTMm5yAcPg0dgc3GPt-ECFxxL8iH0x1FDTYxreVr8pQ")
+BOT_TOKEN = "8495788801:AAH52uGWsD-OUoTDdZlV6oy8NnyduVOmyos"
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0Z19pZCI6NTMxMDU1NTUzNSwiZGJfbm0iOiJzdWJfZGF0YTIyIn0.lTMm5yAcPg0dgc3GPt-ECFxxL8iH0x1FDTYxreVr8pQ"
 
 API_URL = "https://public-api.undresstool.fun/undress"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Gửi ảnh để xử lý 🚀")
+    bot.reply_to(message, "Gửi ảnh để xử lý.")
+
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     try:
         bot.send_message(message.chat.id, "⏳ Đang xử lý...")
 
-        # tải ảnh từ telegram (không lưu file)
+        # tải ảnh từ telegram
         file_info = bot.get_file(message.photo[-1].file_id)
         downloaded = bot.download_file(file_info.file_path)
 
         image_file = io.BytesIO(downloaded)
         image_file.name = "image.jpg"
 
-        # gửi lên API
+        # gửi ảnh lên API
         response = requests.post(
             API_URL,
             headers={
@@ -42,7 +43,7 @@ def handle_photo(message):
             bot.send_message(message.chat.id, f"❌ API lỗi: {response.status_code}")
             return
 
-        # nếu API trả ảnh trực tiếp
+        # nếu API trả về ảnh trực tiếp
         if "image" in response.headers.get("Content-Type", ""):
             result_image = io.BytesIO(response.content)
             result_image.name = "result.jpg"
@@ -65,4 +66,6 @@ def handle_photo(message):
     except Exception as e:
         bot.send_message(message.chat.id, f"⚠️ Lỗi: {str(e)}")
 
+
+print("Bot đang chạy...")
 bot.infinity_polling()
